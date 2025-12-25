322f1bfea91f6eb8a9ca6f6a518635437074a94a
bobbramillan <bobbramillan@gmail.com>
2025-12-24 19:04:28 -0500

    Initial Commit

---
+++ NYCWiFiFinder/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NYCWiFiFinder
+//
+//  Created by Bavanan Bramillan on 12/24/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NYCWiFiFinder/NYCWiFiFinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  NYCWiFiFinderApp.swift
+//  NYCWiFiFinder
+//
+//  Created by Bavanan Bramillan on 12/24/25.
+//
+
+import SwiftUI
+
+@main
+struct NYCWiFiFinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
